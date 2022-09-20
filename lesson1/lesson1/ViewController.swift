fe94fa8dfd3e1e68591054ff168b5cc45815e05d
jinays <jin315@purdue.edu>
2022-09-19 20:03:46 -0400

    lesson 1&lesson2

---
+++ lesson1/lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by 金阿依舍 on 2022/9/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
